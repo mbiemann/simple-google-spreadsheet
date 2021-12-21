cb320786940028b7038da3fcac98a0d65cf37644
Marcell Biemann <mbiemann@gmail.com>
2021-12-21 20:19:31 -0300

    Initial commit

---
+++ simple_google_spreadsheet/__init__.py
@@ -0,0 +1,7 @@
+from simple_google_spreadsheet.spreadsheet import (
+    SpreadSheet
+)
+
+__all__ = [
+    'SpreadSheet'
+]
---
+++ simple_google_spreadsheet/spreadsheet.py
@@ -0,0 +1,17 @@
+import pickle
+import google_auth_oauthlib
+from googleapiclient.discovery import build
+
+class SpreadSheet():
+
+    def __init__(self, spreadsheet_id, client_id, client_secret):
+
+        self._spreadsheet_id = spreadsheet_id
+
+        credentials = google_auth_oauthlib.get_user_credentials(
+            scopes=['https://www.googleapis.com/auth/spreadsheets'],
+            client_id=client_id,
+            client_secret=client_secret
+        )
+
+        self._service = build('sheets','v4',credentials=credentials).spreadsheets().values()
